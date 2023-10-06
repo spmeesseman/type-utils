038e6b591cfb7746c568c3e6d0a322f52bfb379f
Scott Meesseman <spmeesseman@gmail.com>
2023-10-06 04:57:27 -0400

    broken: <3it.should.a.been.me<3iwmbb<3i.m.broken4ever|<t^k3.m3.p1353>lost-n<3broken<3 fng-h8-mi537f

--- src/object.js
+++ src/object.js
@@ -6,7 +6,6 @@ const { isDate, isArray, isObject } = require("./type");
 /**
  * @file src/object.js
  * @version 0.0.1
- * @license MIT
  * @copyright Scott P Meesseman 2023
  * @author Scott Meesseman @spmeesseman
  *//** */
